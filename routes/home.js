4b8ff6c44741478fbe0da4ad523818add56cde21
Simerjeet Mudhar <simerkm@gmail.com>
2024-04-17 18:38:11 -0400

    nav bar inclusion

--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,11 @@ router
   .route('/')
   .get(async (req, res) => {
     res.render('home')
+  }),
+  router
+  .route('/profile')
+  .get(async (req, res) => {
+    res.render('userProfile')
   })
 
 export default router;
