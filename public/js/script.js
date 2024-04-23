c5bb734640ef8e8965d601d353859c537ac06656
Simerjeet Mudhar <simerkm@gmail.com>
2024-04-23 14:23:33 -0400

    popup form

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,30 @@ const addBadgeBtn = document.getElementById('addBadge');
 const formPopup = document.getElementById('formPopup');
 const overlay = document.getElementById('overlay');
 
-// Function to show the form popup
 function openForm() {
     formPopup.style.display = 'block';
     overlay.style.display = 'block';
 }
 
-// Function to close the form popup
 function closeForm() {
     formPopup.style.display = 'none';
     overlay.style.display = 'none';
 }
 
-// Add event listener to the button
 addBadgeBtn.addEventListener('click', openForm);
+
+const addWishBtn = document.getElementById('addWish');
+const wishFormPopup = document.getElementById('wishformPopup');
+// const overlay = document.getElementById('overlay');
+
+function openWishForm() {
+    wishFormPopup.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+function closeWishForm() {
+    wishFormPopup.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+addWishBtn.addEventListener('click', openWishForm);
