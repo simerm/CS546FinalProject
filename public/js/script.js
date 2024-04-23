728f69c4ead9b1d678badf7a89688110e671214f
Simerjeet Mudhar <simerkm@gmail.com>
2024-04-23 17:59:58 -0400

    popup form stylizing

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,10 @@ const overlay = document.getElementById('overlay');
 function openForm() {
     formPopup.style.display = 'block';
     overlay.style.display = 'block';
+    document.getElementById('badgeName').value = ''
+    let error = document.getElementById('clientError')
+    error.innerHTML = ""
+
 }
 
 function closeForm() {
@@ -16,11 +20,13 @@ addBadgeBtn.addEventListener('click', openForm);
 
 const addWishBtn = document.getElementById('addWish');
 const wishFormPopup = document.getElementById('wishformPopup');
-// const overlay = document.getElementById('overlay');
 
 function openWishForm() {
     wishFormPopup.style.display = 'block';
     overlay.style.display = 'block';
+    document.getElementById('wishName').value = ''
+    let error = document.getElementById('wishclientError')
+    error.innerHTML = ""
 }
 
 function closeWishForm() {
@@ -29,3 +35,64 @@ function closeWishForm() {
 }
 
 addWishBtn.addEventListener('click', openWishForm);
+
+document.getElementById('badgeForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let value = true;
+    let badge = document.getElementById("badgeName").value
+    let error = document.getElementById('clientError')
+    error.innerHTML = ""
+    if (!badge || !isNaN(badge)){
+        error.innerHTML = "Must provide a badge name"
+        value = false
+    }
+    else{
+        if (typeof badge !== "string"){
+            error.innerHTML = "Must provide a string"
+            value = false
+
+        }
+        else{
+            badge = badge.trim()
+            if (badge.length < 5){
+                error.innerHTML = "Badge name can't be less than 5 characters"
+                value = false
+            }
+        }
+    }
+    if (value){
+        document.getElementById('badgeForm').submit()
+    }
+
+});
+
+document.getElementById('wishForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let value = true;
+    let wish = document.getElementById("wishName").value
+    let error = document.getElementById('wishclientError')
+    error.innerHTML = ""
+    if (!wish || !isNaN(wish)){
+        error.innerHTML = "Must provide a wish name"
+        value = false
+    }
+    else{
+        if (typeof wish !== "string"){
+            error.innerHTML = "Must provide a string"
+            value = false
+        }
+        else{
+            wish = wish.trim()
+            if (wish.length < 2){
+                error.innerHTML = "Wish name can't be less than 2 characters"
+                value = false
+            }
+        }
+    }
+    if (value){
+        document.getElementById('wishForm').submit()
+    }
+
+});
