4e0ce0ebce0d7ce65f435dab86e0c91c49c16346
Jo-Anne Rivera <riverajoanne231@gmail.com>
2024-04-27 02:35:41 -0400

    added GET /collections route to parse JSON data & imported bootstrap CSS/JS

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,7 @@
 import {Router} from 'express';
 const router = Router();
+import fs from 'fs';
+import path from 'path';
 
 router
   .route('/')
@@ -7,14 +9,24 @@ router
     res.render('home')
   }),
   router
-  .route('/profile')
-  .get(async (req, res) => {
-    res.render('userProfile')
-  }),
-  router
-  .route('/collections')
-  .get(async (req, res) => {
-    
-  })
+    .route('/profile')
+    .get(async (req, res) => {
+      res.render('userProfile')
+    }),
+  router.get('/collections', async (req, res) => {
+    fs.readFile('./data/figurines.json', 'utf8', (err, figurines) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      fs.readFile('./data/series.json', 'utf8', (err, series) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        res.render('collections', {figurines: JSON.parse(figurines), series: JSON.parse(series)})
+      })
+    })
+  });
 
 export default router;
